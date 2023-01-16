79abb7328677b054c35242811e82117ab11fd3b9
Rizqulloh Rifqi Edwanto <111821733+TruflleBlack@users.noreply.github.com>
2023-01-16 09:56:12 +0700

    Create excerpts

---
+++ src/script/data/excerpts.js
@@ -0,0 +1,54 @@
+const excerpts = [
+    {
+        anime: 'Chihayafuru',
+        character: 'Hidehiro Harada',
+        quote: 'Frustrations will not last forever. Nobody can keep on going without some measure of reward.',
+    },
+    {
+        anime: 'Detective Conan',
+        character: 'Jodie Starling',
+        quote: 'Relying on someone else without exerting any effort of your own is just not right. Because the one you rely on most, can disappear, at any time.',
+    },
+    {
+        anime: 'My Hero Academia',
+        character: 'Shigaraki Tomura',
+        quote: 'Heroes and villains both thrive on violence, but we are still categorized. You are good You are evil. That is how it is! Symbol of peace? Hah! In the end you are just a tool for violence, made to keep us down. And violence only breeds more violence.',
+    },
+    {
+        anime: 'Mirai Nikki',
+        character: 'Akise Aru',
+        quote: 'If you want to know what others think, you first have to say what you think.',
+    },
+    {
+        anime: 'Fullmetal Alchemist',
+        character: 'Edward Elric',
+        quote: 'WHO ARE YOU CALLING A MOUSY LITTLE PIPSQUEAK?!',
+    },
+    {
+        anime: 'Dansai Bunri no Crime Edge',
+        character: 'Haimura Kiri',
+        quote: 'If you do not interact with the people around you, it did be hard for anyone to approach you.',
+    },
+    {
+        anime: 'Code Geass: Lelouch of the Rebellion',
+        character: 'Milly Ashford',
+        quote: 'Things that do not change go extinct.',
+    },
+    {
+        anime: 'Shaman King',
+        character:' Anna Kyoyama',
+        quote: 'I do not wear these beads just to make a fashion statement. These are my spirit beads - my link to the afterlife.',
+    },
+    {
+        anime: 'Kingdom Hearts',
+        character: 'Sora',
+        quote: 'The heart may be weak and sometimes it may even give in, but I have learned that deep down, there is a light that never goes out!',
+    },
+    {
+        anime: 'Log Horizon',
+        character: 'Rayneshia El-Arte Corwen',
+        quote: 'Adventurers are free. They are more free than we are. It is true that we may be weaker than they are. But that does not mean that, in our weakness, we can become aggressive. We do not have the right to take advantage of our weakness and use them as tools!',
+    }
+];
+
+export default excerpts;
